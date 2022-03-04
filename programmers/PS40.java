fc0a5b4a95f9cdc6a8f0026186ec2757f276d30c
CHANGMIN LEE <36255290+999inim@users.noreply.github.com>
2022-03-04 22:46:40 +0900

    [programmers] 전화번호부
    https://timotimo.tistory.com/3

---
+++ programmers/PS40.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        ArrayList<String> keyList = new ArrayList<String>();        
+       
+        Arrays.sort(phone_book);
+        
+        for(int i=0; i<phone_book.length-1; i++) {
+                if(phone_book[i+1].indexOf(phone_book[i])==0){
+                    answer = false;
+                    break;
+                }
+        }
+            
+        return answer;
+    }
+}
